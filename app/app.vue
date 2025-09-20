25630059738f4b34793bb8fcd55767f13d81e4c6
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-20 18:58:32 +0200

    FEATURE: Add Sass and working menu

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,13 @@
 <template>
+	<Html>
+		<Head>
+			<Title>Info Site</Title>
+		</Head>
+	</Html>
 	<div class="p-6 bg-slate-600 text-slate-200 min-h-screen">
-		<h1 class="text-4xl font-bold mb-3">Info Site</h1>
-		<h2 class="-mt-3 text-xs italic text-yellow-200 mb-3">this site made with <a class="underline" target="_blank" href="https://nuxt.com/docs/4.x/getting-started/introduction">Nuxt 4</a>, see <a class="underline" target="_blank" href="https://github.com/edwardtanguay/nuxt-showcase-001">the code</a></h2>
-		<p>Welcome to this site.</p>
+	<Header />
+	<Nav/>
+	<NuxtPage />
 	</div>
+
 </template>
